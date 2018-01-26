0bc1bddac8d97f1355d2a172a412d178141ac6a8
Mats Lidell <mats.lidell@cag.se>
2018-01-27 00:34:27 +0100

    Add license line GPL

--- dbk.el
+++ dbk.el
@@ -2,6 +2,8 @@
 
 ;; Copyright (C) 2006 Elena Pomohaci (epomulet)
 
+;; License: GPL
+
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
 ;; by the Free Software Foundation.
